0096a11e15a3489cabe209846e66a3fe057dc8a2
ruhap <8826243+ruhap@users.noreply.github.com>
2022-11-21 11:26:37 +0200

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,7 +7,7 @@ type LayoutProps = {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-h-screen">
-      <main className="w-12/12 xl: mx-auto flex w-10/12 flex-col p-2 md:w-8/12 md:p-4">
+      <main className="w-12/12 mx-auto flex flex-col p-2 md:w-8/12 md:p-4 xl:w-10/12">
         {children}
       </main>
     </div>
